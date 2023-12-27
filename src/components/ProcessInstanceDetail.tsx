f7c4c09f95c61d1ad24517441be2e08521abc62a
Jiří Vrba <jiri@vrba.dev>
2023-12-27 16:12:28 +0100

    Implement zooming and scrolling within the workflow diagram

--- src/components/ProcessInstanceDetail.tsx
+++ src/components/ProcessInstanceDetail.tsx
@@ -1,5 +1,5 @@
 import {ProcessInstance} from "../types/ProcessInstance.ts";
-import {FC, useMemo} from "react";
+import {FC, useEffect, useMemo, useRef} from "react";
 import {ProcessInstanceStateIcon} from "./ProcessInstanceStateIcon.tsx";
 import {stateBorderColors, stateTextColors} from "../helpers/colors.ts";
 import {NavLink, useLocation} from "react-router-dom";
@@ -8,6 +8,7 @@ import dateformat from "dateformat";
 import {Button, Chip} from "@nextui-org/react";
 import {MermaidGraph} from "./MermaidGraph.tsx";
 import {buildMermaidSourceFromJson} from "../helpers/graph.ts";
+import {ReactZoomPanPinchRef, TransformComponent, TransformWrapper} from "react-zoom-pan-pinch";
 
 export type ProcessInstanceDetailProps = {
     instance: ProcessInstance;
@@ -17,17 +18,25 @@ export type ProcessInstanceDetailProps = {
 export const ProcessInstanceDetail: FC<ProcessInstanceDetailProps> = ({instance, reload}) => {
     const {pathname} = useLocation()
     const graph = useMemo(() => buildMermaidSourceFromJson(instance.source), [instance.source]);
+    const graphTransformRef = useRef<ReactZoomPanPinchRef | null>(null);
+
+    useEffect(() => {
+        setTimeout(() => {
+            graphTransformRef.current?.centerView();
+            graphTransformRef.current?.resetTransform();
+        }, 100);
+    }, []);
 
     return (
-        <div className={`${stateBorderColors[instance.state]} flex flex-col items-stretch justify-start border-t-4 p-8`}>
+        <div className={`${stateBorderColors[instance.state]} flex flex-col items-stretch justify-start border-t-4 p-8 min-h-screen`}>
             <header className="flex flex-row items-center justify-between pb-4 mb-4 border-b-1 border-default">
                 <div>
                     <NavLink to={pathname.split("/instance/")[0]} className="text-xs opacity-50 hover:opacity-100">Return to process instances listing</NavLink>
                     <div className="flex flex-row items-center justify-start gap-4 my-2">
                         <h1 className="text-3xl font-bold my-2">{instance.processName}</h1>
                         <div className={`${stateTextColors[instance.state]} flex flex-row items-center justify-start gap-2`}>
-                        <ProcessInstanceStateIcon state={instance.state}/>
-                        {instance.state}
+                            <ProcessInstanceStateIcon state={instance.state}/>
+                            {instance.state}
                         </div>
                     </div>
                     <div>
@@ -58,13 +67,22 @@ export const ProcessInstanceDetail: FC<ProcessInstanceDetailProps> = ({instance,
                     <Button color="primary" className="mt-5" onPress={reload}>Reload data</Button>
                 </div>
             </header>
-            <main>
-                TIMELINE
+            <main className="grid grid-row grid-cols-4 flex-grow">
+                <div>
+                    TIMELINE
+                </div>
 
-                <MermaidGraph source={graph}/>
+                <div>
+                    VARIABLES
+                </div>
 
-                GRAPH
-                VARIABLES
+                <div className="col-span-2 h-full [&>div]:w-full [&>div]:h-full">
+                    <TransformWrapper ref={graphTransformRef}>
+                        <TransformComponent>
+                            <MermaidGraph source={graph}/>
+                        </TransformComponent>
+                    </TransformWrapper>
+                </div>
             </main>
         </div>
     )
