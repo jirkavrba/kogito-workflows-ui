bfc9afa49f5ac6cc86ade69a12d8826847ec721a
Jiří Vrba <jiri@vrba.dev>
2024-01-01 09:19:24 +0100

    Add fetching indicator to the top right corner

--- src/components/ServerNavbar.tsx
+++ src/components/ServerNavbar.tsx
@@ -1,13 +1,17 @@
-import {NavLink} from "react-router-dom";
-import {FC} from "react";
-import {ServerConfiguration} from "../types/ServerConfiguration.ts";
-import {Button} from "@nextui-org/react";
+import { NavLink } from "react-router-dom";
+import { FC } from "react";
+import { ServerConfiguration } from "../types/ServerConfiguration.ts";
+import { Button, Spinner } from "@nextui-org/react";
+import { useIsFetching } from "@tanstack/react-query";
+import { LuUnplug } from "react-icons/lu";
 
 export type ServerNavbarProps = {
     configuration: ServerConfiguration;
 }
 
-export const ServerNavbar: FC<ServerNavbarProps> = ({configuration}) => {
+export const ServerNavbar: FC<ServerNavbarProps> = ({ configuration }) => {
+    const fetching = useIsFetching();
+
     return (
         <header className="flex flex-row justify-between items-center px-8">
             {/*<Flex p={5} direction="row" justifyContent="space-between" alignItems="center">*/}
@@ -17,7 +21,11 @@ export const ServerNavbar: FC<ServerNavbarProps> = ({configuration}) => {
                 <code className="text-xs text-gray-500">{configuration.url}</code>
             </div>
             <NavLink to="/">
-                <Button>Disconnect</Button>
+                {fetching > 0 && <Spinner size="sm" className="mr-2" />}
+                <Button>
+                    <LuUnplug />
+                    Disconnect
+                </Button>
             </NavLink>
         </header>
     );
