d774b7eb1604482554f3fdb40d6ff53d5ba39107
Jiří Vrba <jiri@vrba.dev>
2024-01-05 19:21:33 +0100

    Add a link to the Gitlab repository

--- src/pages/server.tsx
+++ src/pages/server.tsx
@@ -9,6 +9,7 @@ import {Spinner} from "@nextui-org/react";
 import {useProcessDefinitions} from "../shared/useProcessDefinitions.tsx";
 import {ProcessInstancesFilter} from "../components/ProcessInstancesFilter.tsx";
 import {ProcessInstanceState} from "../types/ProcessInstance.ts";
+import {LuGitlab} from "react-icons/lu";
 
 const Loader: FC = () => {
     return (
@@ -55,6 +56,17 @@ export const ServerPage: FC = () => {
                                     setProcessStates(processStates);
                                 }}/>
                     }
+                    <div className="flex flex-row justify-center items-center my-2 gap-2">
+                        <span className="text-xs text-neutral-700">&copy; Jiří Vrba {new Date().getFullYear()}</span>
+                        <span className="text-neutral-500">
+                            <LuGitlab/>
+                        </span>
+                        <span className="text-xs">
+                            <a href="https://gitlab.com/jirkavrba/kogito-workflows-ui" target="_blank" className="text-neutral-700 hover:text-orange-500">
+                                Source code
+                            </a>
+                        </span>
+                    </div>
                 </div>
                 <div className="col-span-2">
                     {
