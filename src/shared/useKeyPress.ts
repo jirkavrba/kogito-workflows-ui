048d147c3f1425a67d97cb7b93ab3cf4d5927bb7
Jiří Vrba <jiri@vrba.dev>
2024-04-25 12:55:06 +0200

    Temporarily disabled key press handlers

--- src/shared/useKeyPress.ts
+++ src/shared/useKeyPress.ts
@@ -2,13 +2,15 @@ import { useEffect } from "react"
 
 export const useKeyPress = (key: string, callback: (event: KeyboardEvent) => void) => {
     useEffect(() => {
-        const handler = (event: KeyboardEvent) => {
-            if (event.key == key) {
-                callback(event);
-            }
-        };
-
-        window.addEventListener("keyup", handler)
-        return () => window.removeEventListener("keyup", handler);
+        // Temporarily disabled
+        //
+        // const handler = (event: KeyboardEvent) => {
+        //     if (event.key == key) {
+        //         callback(event);
+        //     }
+        // };
+        //
+        // window.addEventListener("keyup", handler)
+        // return () => window.removeEventListener("keyup", handler);
     }, [callback, key]);
 }
