23d5012c61dfcaee2577ec538b501dee058054c6
Jiří Vrba <jiri@vrba.dev>
2024-08-20 17:57:30 +0200

    Typo

--- src/components/detail/variables/ProcessInstanceVariablesPlaceholder.tsx
+++ src/components/detail/variables/ProcessInstanceVariablesPlaceholder.tsx
@@ -7,7 +7,7 @@ export const ProcessInstanceVariablesPlaceholder: FC = () => {
     return (
         <Card className="mx-4">
             <CardBody className="text-center py-8">
-                <h1 className="text-xl font-bold text-center mx-3">Variable editor is not enabled</h1>
+                <h1 className="text-xl font-bold text-center mx-3">Variables editor is not enabled</h1>
                 <span>This feature needs to be enabled in the settings.</span>
 
                 <NavLink to="/settings" className="mt-8">
